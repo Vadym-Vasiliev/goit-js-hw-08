c5f8efa2a38e2d2b7a4b9444f60611a12b890393
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-06-03 10:59:51 +0300

    add finish 8 module

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,24 @@
 // Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message,
 // у яких зберігай поточні значення полів форми.Нехай ключем для сховища буде рядок "feedback-form-state".
 
+import throttle from 'lodash.throttle';
+
 const feedBackForm = document.querySelector('.feedback-form');
 const FORM_KEY = 'feedback-form-state';
-feedBackForm.addEventListener('submit', formButtonSubmit);
-feedBackForm.addEventListener('input', evt => {
-  const { name, value } = evt.target;
-  let saveDate = JSON.parse(localStorage.getItem(FORM_KEY)) ?? {};
-  saveDate[name] = value;
 
-  localStorage.setItem(FORM_KEY, JSON.stringify(saveDate));
-});
+feedBackForm.addEventListener('submit', formButtonSubmit);
+
+messageInput();
+feedBackForm.addEventListener(
+  'input',
+  throttle(evt => {
+    const { name, value } = evt.target;
+    let saveDate = JSON.parse(localStorage.getItem(FORM_KEY)) ?? {};
+    saveDate[name] = value;
+
+    localStorage.setItem(FORM_KEY, JSON.stringify(saveDate));
+  }, 500)
+);
 
 //Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми.
 // В іншому випадку поля повинні бути порожніми.
@@ -19,7 +27,6 @@ function messageInput() {
   const savedMassage = localStorage.getItem(FORM_KEY);
 
   if (savedMassage) {
-    console.log(JSON.parse(savedMassage));
     Object.entries(JSON.parse(savedMassage)).forEach(([name, value]) => {
       feedBackForm.elements[name].value = value;
     });
@@ -31,17 +38,15 @@ function messageInput() {
 
 function formButtonSubmit(evt) {
   evt.preventDefault();
-  console.log('Отправка  форми');
-  evt.currentTarget.reset();
+
+  const saveData = localStorage.getItem(FORM_KEY);
+
+  if (!saveData) return;
+  console.log(JSON.parse(saveData));
+
+  feedBackForm.reset();
+
   localStorage.removeItem(FORM_KEY);
 }
-
-// function onFormSubmit(evt) {
-//   evt.preventDefault();
-//   const formData = new FormData(refs);
-//   formData.forEach((key, value) => console.log(`${value}: ${key}`));
-//   evt.currentTarget.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-// }
 
 //Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
